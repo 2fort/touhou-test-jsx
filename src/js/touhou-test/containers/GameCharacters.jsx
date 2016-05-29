e65823cc64d2c21518dae9dd09dc3783c7dda042
Alexander Kirsanov <nullcallback@gmail.com>
2016-05-29 11:33:33 -0700

    home page improved

--- src/js/touhou-test/containers/GameCharacters.jsx
+++ src/js/touhou-test/containers/GameCharacters.jsx
@@ -1,23 +1,15 @@
-import React, { Component, PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import Link from 'react-router/lib/Link';
+import React, { PropTypes } from 'react';
 import snakeCase from 'lodash/lowerCase';
 import DocumentTitle from 'react-document-title';
 
-import * as CharactersActions from '../actions/charactersActions';
-
-const GameCharacters = ({ location, params }) => {
-    return (
-        <DocumentTitle title="Game | Touhou">
-            <div className="simple-container">
-            wut <br />
-            {snakeCase(location.pathname)} <br />
-            {snakeCase(params.game)} <br />
-            </div>
-        </DocumentTitle>
-    ); 
-}
+const GameCharacters = ({ location, params }) => (
+    <DocumentTitle title="Game | Touhou">
+        <div className="simple-container">
+            pathname: {location.pathname} <br />
+            params: {snakeCase(params.game)} <br />
+        </div>
+    </DocumentTitle>
+);
 
 GameCharacters.propTypes = {
     params: PropTypes.object,
